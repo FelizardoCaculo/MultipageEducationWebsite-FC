fb383aa2c2e10a81869fccf83088b7102217ad66
FelizardoCaculo <felizardo1987@gmail.com>
2024-09-01 09:27:25 +0100

    Add Courses and FAQs sections

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
 //change navbar styles on scroll
 
 window.addEventListener('scroll', () => {
-    document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0); //to change imediatly when scrolling, we set to 0
+    document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0); //to change imediatly when scrolling, we set to 0      
+});
+
+//Show/Hide faq ansewr
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+
+        //Change icon
+        const icon = faq.querySelector('.faq__icon i');
+        if(icon.className === 'uil uil-plus'){
+            icon.className = 'uil uil-minus';
+        } else{
+            icon.className = 'uil uil-plus      '
+        }
+    })
 })
